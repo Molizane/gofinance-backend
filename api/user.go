607ce325d5e759ece804926c8e689ac209518d07
Molizane <molizane@gmail.com>
2023-04-29 10:41:49 -0300

    Alterações

--- api/user.go
+++ api/user.go
@@ -1,15 +1,10 @@
 package api
 
 import (
-	"bytes"
-	"crypto/sha512"
-	"database/sql"
 	"net/http"
 
 	db "github.com/Molizane/gofinance-backend/db/sqlc"
-
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type createUserRequest struct {
@@ -20,77 +15,17 @@ type createUserRequest struct {
 
 func (server *Server) createUser(ctx *gin.Context) {
 	var req createUserRequest
-	err := ctx.ShouldBindJSON(&req)
-	if err != nil {
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
 	}
 
-	hashedInput := sha512.Sum512_256([]byte(req.Password))
-	trimmedHash := bytes.Trim(hashedInput[:], "\x00")
-	preparedPassword := string(trimmedHash)
-	passwordHashInBytes, err := bcrypt.GenerateFromPassword([]byte(preparedPassword), bcrypt.DefaultCost)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	}
-	var passwordHashed = string(passwordHashInBytes)
 	arg := db.CreateUserParams{
 		Username: req.Username,
-		Password: passwordHashed,
 		Email:    req.Email,
+		Password: req.Password,
 	}
 
 	user, err := server.store.CreateUser(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	}
-
-	ctx.JSON(http.StatusOK, user)
-}
-
-type getUserRequest struct {
-	Username string `uri:"username" binding:"required"`
-}
-
-func (server *Server) getUser(ctx *gin.Context) {
-	var req getUserRequest
-	err := ctx.ShouldBindUri(&req)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-	}
-
-	user, err := server.store.GetUser(ctx, req.Username)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-
-	ctx.JSON(http.StatusOK, user)
-}
-
-type getUserByIdRequest struct {
-	ID int32 `uri:"id" binding:"required"`
-}
-
-func (server *Server) getUserById(ctx *gin.Context) {
-	var req getUserByIdRequest
-	err := ctx.ShouldBindUri(&req)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-	}
-
-	user, err := server.store.GetUserById(ctx, req.ID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-
-	ctx.JSON(http.StatusOK, user)
 }
